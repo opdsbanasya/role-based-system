c46a2c0fc3c8b42789a656e4282ed6869a344980
DHARM <opdsbanasya@gmail.com>
2025-07-31 20:33:56 +0530

    FIXED: email validation issue

--- backend/src/utils/validate.js
+++ backend/src/utils/validate.js
@@ -22,10 +22,17 @@ const validateSignupData = async (data) => {
   return data;
 };
 
-const validateLoginData = (data) => {
-  if (!validator.isEmail(data.email)) {
+const validateLoginData = (email) => {
+  if (!validator.isEmail(email)) {
     throw new Error("Invalid Email");
   }
 };
 
-module.exports = { validateSignupData, validateLoginData };
+const validateUpdatePassword = (newPassword) => {
+  if(!validator.isStrongPassword(newPassword)){
+    throw new Error("Password length must be at least 8 and include at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character.")
+  }
+
+}
+
+module.exports = { validateSignupData, validateLoginData, validateUpdatePassword };
